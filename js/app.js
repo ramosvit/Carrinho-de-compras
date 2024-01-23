c200fa7063fc4c454e5681e1f13ee93a1592c4ff
Vitor Araujo Ramos <vitor.ramos.ara@gmail.com>
2024-01-23 19:04:19 -0300

    carrinho-de-compras

---
+++ js/app.js
@@ -0,0 +1,32 @@
+let totalGeral;
+limpar(); //funcao realizada na linha 27, reinica todos os valores 
+
+function adicionar (){
+    //recuperar valores do produto, quantidade e valor
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0]; //o [0] representa a posicao que o nome do produto está
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
+    
+    // calcular o preco, o nosso subtotal
+    let preco  = quantidade * valorUnitario;
+
+    //adicionar no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + ` <section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+  </section>`;
+
+    //atualizar o valor total
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0; //limpa quantidade de produtos que coloquei
+   
+}
+
+function limpar(){
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = ''; //está informando que o carrinho está vazio para quando reiniciar nao contabilizar hist
+    document.getElementById('valor-total').innerHTML = 'R$ 0';
+}
